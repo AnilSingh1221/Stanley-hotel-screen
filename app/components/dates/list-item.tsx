fa7624c6ce2667f8fa9480343c90f2029ef476a4
Nathanael Merrill <nathanael@webconnex.com>
2023-07-25 17:50:11 -0500

    fixing strike through on multiple events

--- app/components/dates/list-item.tsx
+++ app/components/dates/list-item.tsx
@@ -8,9 +8,17 @@ interface Props {
 }
 
 const ListItem:React.FC<Props> = ({label, remaining, type}) => {
+
+	const formatLabel = (label: string, remaining: number) => {
+		if (remaining === 0) {
+			return <span className="line-through decoration-gray-500">{label}</span>
+		}
+		return label
+	}
+
 	return (
 		<li className="px-2 py-1.5">
-			{label} <Status remaining={remaining} type={type} />
+			{formatLabel(label as string, remaining)} <Status remaining={remaining} type={type} />
 		</li>
 	)
 }
