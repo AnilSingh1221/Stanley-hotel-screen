4747ba35fba3da2296e57eb62eb8024ff579abf1
Nathanael Merrill <nathanael@webconnex.com>
2023-12-04 10:34:23 -0600

    Updating layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata = {
   title: 'The Stanley Hotel',
   description: 'Stanley Hotel In Estes Park, CO offers 4 types of hotel accommodations including 1909 historic hotel rooms, historic boutique hotel, modern hotel rooms, and 1 to 3 bedroom condominiums perfect for groups. Enjoy stunning views of the Rocky Mountains.',
 }
-
+// main layout
 export default function RootLayout({
   children,
 }: {
